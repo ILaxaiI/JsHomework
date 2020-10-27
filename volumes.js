2f9153d47952c5ebed0c7329f5b39577d5941f39
iLaxaii <lukas.fath02@gmail.com>
2020-10-27 16:27:38 +0100

    anim

--- volumes.js
+++ volumes.js
@@ -1,27 +1,29 @@
-var formulas = {
-    rectangle(w,h){
+var formulas = [
+    
+    function circle(r){
+        return Math.pi*r*r
+    },
+    function sphere(r){
+        return Math.pi*(4/3)*r*r*r
+    },
+    function cylinder(r,h){
+        return Math.pi*r*r*h
+    },  
+    function rectangle(w,h){
         return w*h
     },
-    cuboid(w,h,d){
+    function cuboid(w,h,d){
         return w*h*d
     },
 
-    circle(r){
-        return Math.pi*r*r
-    },
-    sphere(r){
-        return Math.pi*(4/3)*r*r*r
-    },
-
-    trapezoid(a,c,h){
+    
+    function trapezoid(a,c,h){
         return (a+c)/2*h
     },
-    cylinder(r,h){
-        return Math.pi*r*r*h
-    },
 
-    pyramid(a,b,h){
+
+    function pyramid(a,b,h){
         return (1/3)*a*b*h
     }
-}
+];
 
